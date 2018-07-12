59d4a2a2cdc16d776d54786c034f030554697b84
marclll <sgmarcsg@gmail.com>
2018-07-12 22:21:20 +0200

    poketable

--- ajax_poketable.js
+++ ajax_poketable.js
@@ -1,6 +1,6 @@
 var id=1;
 
-$().ready(function(){
+$(function(){
 	$.get("http://40.118.8.76/pokemons",
 		function(data){
 			$("#code").text(data[0].num);
@@ -8,7 +8,9 @@ $().ready(function(){
 			$("#shiny").attr("src",data[0].images[1].front_shiny);
 			$("#name").text(data[0].name);
 			$("#type").text(data[0].types[0].type.name+" & "+data[0].types[1].type.name);
-		},"json")
+			if (data[0].moves.HM!=null) $("#hm").text(data[0].moves.HM[0]);
+			else $("#hm").text("null");
+		},"json");
 
 		$("#prev").click(function(){
 			--id;
@@ -23,7 +25,50 @@ $().ready(function(){
 		});
 
 		$("#search").click(function(){
-			
+			var x=$("#poke").val();
+			var find=false;
+			$.get("http://40.118.8.76/pokemons/?num="+x,
+				function(data){
+					if (data.length!=0){
+						$("#code").text(data[0].num);
+						$("#normal").attr("src",data[0].images[0].front);
+						$("#shiny").attr("src",data[0].images[1].front_shiny);
+						$("#name").text(data[0].name);
+						var n=data[0].types.length;
+						$("#type").text(data[0].types[0].type.name);
+						for (i=1;i<n;++i){
+							$("#type").append(" & "+data[0].types[i].type.name);
+						}
+						if (data[0].moves.HM!=null) $("#hm").text(data[0].moves.HM[0]);
+						else $("#hm").text("null");
+						find=true;
+					}
+				},"json")
+
+			.done(function(){
+				if (!find){
+					$.get("http://40.118.8.76/pokemons/?name="+x,
+					function(data){
+						if (data.length!=0){
+							$("#code").text(data[0].num);
+							$("#normal").attr("src",data[0].images[0].front);
+							$("#shiny").attr("src",data[0].images[1].front_shiny);
+							$("#name").text(data[0].name);
+							var n=data[0].types.length;
+							$("#type").text(data[0].types[0].type.name);
+							for (i=1;i<n;++i){
+								$("#type").append(" & "+data[0].types[i].type.name);
+							}
+							if (data[0].moves.HM!=null) $("#hm").text(data[0].moves.HM[0]);
+							else $("#hm").text("null");
+							find=true;					}
+					},"json")
+					.done(function(){
+						if (!find && x!="") alert("Invalid name or code");
+					});
+				}
+			});
+			$("#poke").val("");
 		});
 });
 
@@ -37,8 +82,10 @@ function act(){
 			$("#name").text(data.name);
 			var n=data.types.length;
 			$("#type").text(data.types[0].type.name);
-			for (i=1;i<n;++i){
+			for (var i=1;i<n;++i){
 				$("#type").append(" & "+data.types[i].type.name);
 			}
+			if (data.moves.HM!=null) $("#hm").text(data.moves.HM[0]);
+			else $("#hm").text("null");
 		},"json");
 }
